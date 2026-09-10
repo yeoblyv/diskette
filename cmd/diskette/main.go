dd2c0dc2ec504baed948c101426fcfb74bed8121
Yehor Oblyvantsov <xineraman8@gmail.com>
2026-09-10 23:03:37 +0200

    fix: mention FTP/FTPS in the About dialog now that it's real too

--- cmd/diskette/main.go
+++ cmd/diskette/main.go
@@ -1337,7 +1337,7 @@ func showAbout(app *Graphite.Application) {
 	info := Graphite.NewLabel(40, 2,
 		"Diskette v.0.1.0\n\n"+
 			"A cross-platform dual-pane file manager,\n"+
-			"with SFTP remote connections.\n"+
+			"with SFTP and FTP/FTPS remote connections.\n"+
 			"Provides archiving features and build with\n"+
 			"lightweight Graphite TUI framework.\n\n"+
 			"Copyright © 2026 Yehor Oblyvantsov\n"+
